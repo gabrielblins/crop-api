2f3b6fe0ceb106e668da7b5877dc40017bebf36c
Gabriel Lins <academic.gabrielblins@gmail.com>
2023-10-08 23:01:09 -0300

    Updated main API file

--- main.py
+++ main.py
@@ -9,25 +9,20 @@ from dotenv import load_dotenv
 import os
 load_dotenv();
 
-# Create FastAPI app
 app = FastAPI()
 
-# Database connection settings
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_BD = os.getenv("DB_BD")
 
-# SQLAlchemy database connection
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_BD}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Load IBGE municipios data from JSON file
 with open("ibge_municipios.json", "r") as json_file:
     ibge_municipios_data = json.load(json_file)
 
-# Define Pydantic models for request and response
 class CropRequest(BaseModel):
     cod_variavel: int
     cod_produto_lavouras_temporarias: int
@@ -35,21 +30,20 @@ class CropRequest(BaseModel):
     cod_municipio: int
 
 class CropResponse(BaseModel):
-    cod_nivel_territorial: List[int]
-    nivel_territorial: List[str]
-    cod_unidade_medida: List[int]
-    unidade_medida: List[str]
-    valor: List[float]
-    cod_municipio: List[int]
-    nome_municipio: List[str]
-    cod_ano: List[int]
-    ano: List[int]
-    cod_produto_lavouras_temporarias: List[int]
-    produto_lavouras_temporarias: List[str]
-    cod_variavel: List[int]
-    variavel: List[str]
+    cod_nivel_territorial: int
+    nivel_territorial: str
+    cod_unidade_medida: int
+    unidade_medida: str
+    valor: float
+    cod_municipio: int
+    nome_municipio: str
+    cod_ano: int
+    ano: int
+    cod_produto_lavouras_temporarias: int
+    produto_lavouras_temporarias: str
+    cod_variavel: int
+    variavel: str
 
-# Define API endpoint to fetch crop data
 @app.get("/crop/", response_model=CropResponse)
 def get_crop_data(
     cod_variavel: int = Query(..., description="Code for the variable"),
@@ -79,33 +73,24 @@ def get_crop_data(
 
         # Convert results to the response model format
         response_data = {
-            "cod_nivel_territorial": [row.cod_nivel_territorial for row in results],
-            "nivel_territorial": [row.nivel_territorial for row in results],
-            "cod_unidade_medida": [row.cod_unidade_medida for row in results],
-            "unidade_medida": [row.unidade_medida for row in results],
-            "valor": [row.valor for row in results],
-            "cod_municipio": [row.cod_municipio for row in results],
-            "nome_municipio": [[i for i in ibge_municipios_data if i['ibge_code'] == cod_municipio][0]['municipio']],
-            "cod_ano": [row.cod_ano for row in results],
-            "ano": [row.ano for row in results],
-            "cod_produto_lavouras_temporarias": [row.cod_produto_lavouras_temporarias for row in results],
-            "produto_lavouras_temporarias": [row.produto_lavouras_temporarias for row in results],
-            "cod_variavel": [row.cod_variavel for row in results],
-            "variavel": [row.variavel for row in results],
+            "cod_nivel_territorial": [row.cod_nivel_territorial for row in results][0],
+            "nivel_territorial": [row.nivel_territorial for row in results][0],
+            "cod_unidade_medida": [row.cod_unidade_medida for row in results][0],
+            "unidade_medida": [row.unidade_medida for row in results][0],
+            "valor": [row.valor for row in results][0],
+            "cod_municipio": [row.cod_municipio for row in results][0],
+            "nome_municipio": [i for i in ibge_municipios_data if i['ibge_code'] == cod_municipio][0]['municipio'],
+            "cod_ano": [row.cod_ano for row in results][0],
+            "ano": [row.ano for row in results][0],
+            "cod_produto_lavouras_temporarias": [row.cod_produto_lavouras_temporarias for row in results][0],
+            "produto_lavouras_temporarias": [row.produto_lavouras_temporarias for row in results][0],
+            "cod_variavel": [row.cod_variavel for row in results][0],
+            "variavel": [row.variavel for row in results][0],
         }
 
         return response_data
 
-# # Define API endpoint to fetch IBGE municipios data by code
-# @app.get("/ibge_municipios/{ibge_code}")
-# def get_ibge_municipio(ibge_code: int):
-#     for municipio in ibge_municipios_data:
-#         if municipio["ibge_code"] == ibge_code:
-#             return municipio
 
-#     return {"message": "Municipio not found for the provided code."}
-
-# Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
 
